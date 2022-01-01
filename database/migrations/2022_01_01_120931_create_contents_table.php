bdad5bebec10b684cbad0066e0f875d3241c05ec
hussenk <hussen199710@yahoo.com>
2022-01-01 14:16:38 +0200

    [Add] Content Model

---
+++ database/migrations/2022_01_01_120931_create_contents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('text');
+            $table->date('publish_date');
+            $table->integer('writer_id');
+            $table->integer('category_id');
+            $table->integer('magazine_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
